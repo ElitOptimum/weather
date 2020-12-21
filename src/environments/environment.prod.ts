fc11ff8ca5442d65d1f1f3ff984a68aaf297e6fa
Nemanja Dj <nemanja.dordevic@typeqast.com>
2020-12-21 16:11:03 +0100

    Add build for git pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherUrl:'https://api.weatherbit.io/v2.0/forecast/daily',
+  apiKey: '4a52b7a611b04726a4efc4c84bf94c36'
 };
